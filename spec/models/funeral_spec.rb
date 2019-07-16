198b6deaf1a2921c4f7bb9dd13940247b275bf57
Brian Plantico <bplantico@gmail.com>
2019-07-16 17:14:14 -0600

    Adds enums for burial_cremation and type_of_service to Funeral model with passing tests.
    Co-Authored-By: Earl Stephens <34906415+earl-stephens@users.noreply.github.com>
    Co-Authored-By: Noah Flint <34421236+n-flint@users.noreply.github.com>
    Co-Authored-By: Ryan Miller <42855291+ryanmillergm@users.noreply.github.com>

--- spec/models/funeral_spec.rb
+++ spec/models/funeral_spec.rb
@@ -4,4 +4,29 @@ RSpec.describe Funeral do
   describe 'validations' do
     it { should validate_presence_of :burial_cremation }
   end
+
+  describe  'enums' do
+    before :each do
+      @funeral = Funeral.create(burial_cremation: 0)
+    end
+
+    it 'A burial funeral can be created' do
+      expect(@funeral.burial_cremation).to eq('burial')
+    end
+
+    it 'Funeral burial_cremation can be changed' do
+      @funeral.update(burial_cremation: 1)
+
+      expect(@funeral.burial_cremation).to eq('cremation')
+    end
+
+    it 'Funeral type of service enum works' do
+      @funeral.update(type_of_service: 0)
+      expect(@funeral.type_of_service).to eq('traditional')
+      @funeral.update(type_of_service: 1)
+      expect(@funeral.type_of_service).to eq('military')
+      @funeral.update(type_of_service: 5)
+      expect(@funeral.type_of_service).to eq('closed_casket')
+    end
+  end
 end
