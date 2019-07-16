e9f458251a4a920f16712679e74d05abdaf0d2b3
Brian Plantico <bplantico@gmail.com>
2019-07-16 16:00:32 -0600

    Adds validations for User model
    Co-Authored-By: Earl Stephens <34906415+earl-stephens@users.noreply.github.com>
    Co-Authored-By: Noah Flint <34421236+n-flint@users.noreply.github.com>
    Co-Authored-By: Ryan Miller <42855291+ryanmillergm@users.noreply.github.com>

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+
+  validates_presence_of :first_name
+  validates_presence_of :last_name
+  validates_presence_of :phone
+  validates_presence_of :email
+  
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe User do
+  describe 'validations' do
+    it { should validate_presence_of :first_name }
+    it { should validate_presence_of :last_name }
+    it { should validate_presence_of :email }
+    it { should validate_presence_of :phone }
+  end
+end
