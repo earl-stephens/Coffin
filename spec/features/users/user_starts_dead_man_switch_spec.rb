6e042c121f88bd4421dc10d29eddc75c6347c095
Ryan Miller <ryanmillergm@gmail.com>
2019-07-20 18:42:47 -0600

    Adds user starts dead man switch test

---
+++ spec/features/users/user_starts_dead_man_switch_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe "As a logged in user" do
+  describe "and I visit the dashboard" do
+    describe "I see a button to activate my dead man switch" do
+      before :each do
+        @user1 = User.create(first_name: "Bob", last_name: "Olsen", email: "bob@gmail.com")
+        @dead_man_switch = DeadManSwitch.create(length_of_time: 7, user_id: @user1.id)
+
+        allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user1)
+      end
+      it "I can activate my dead man switch" do
+        visit dashboard_path
+
+        expect(page).to have_button("Dead Man's Switch")
+
+        click_button("Dead Man's Switch")
+      end
+    end
+  end
+end
