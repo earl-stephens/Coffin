16d5e66859d22c68533228ce4e38f4668f369159
Noah Flint <noah.flint24@gmail.com>
2019-07-21 15:38:09 -0600

    added update action

--- app/controllers/extra_info_controller.rb
+++ app/controllers/extra_info_controller.rb
@@ -9,9 +9,23 @@ class ExtraInfoController < ApplicationController
     @user = User.find(params['id'])
   end
 
-  def update
-    @user = User.find(params['id'])
+  def create
     require 'pry'; binding.pry
+  end 
+
+  def update
+    @user = current_user
+    # user = current_user
+    # require 'pry'; binding.pry
+    # @user.will.attach(File.open('./storage/TestWill.pdf'))
+    # @user.will.attach(params)
+    redirect_to profile_path
   end
+
+  private
+
+    def upload_params
+      params.permit(:will, :authenticity_token, :utf8, :commit, :id)
+    end
 
 end
