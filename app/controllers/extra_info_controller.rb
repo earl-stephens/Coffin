0d73b0b13bf54007b084fca6410f1e56cfa639b5
Noah Flint <noah.flint24@gmail.com>
2019-07-21 11:04:03 -0600

    added index and update action;

--- app/controllers/extra_info_controller.rb
+++ app/controllers/extra_info_controller.rb
@@ -1,7 +1,17 @@
 class ExtraInfoController < ApplicationController
 
-  def edit
+  def index
+    #current_user?
+    @user = User.find(params['id'])
+  end
 
+  def edit
+    @user = User.find(params['id'])
+  end
+
+  def update
+    @user = User.find(params['id'])
+    require 'pry'; binding.pry
   end
 
 end
