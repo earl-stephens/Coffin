e6c1e2cbd5a03ef5abd4e9ed1a4187a16b154517
Brian Plantico <bplantico@gmail.com>
2019-07-23 15:35:54 -0600

    Comments out "import Rails from 'rails-ujs'; Rails.start();" from application.js file. Adds skeleton spec for twilio message notification.

---
+++ spec/features/users/user_receives_notifications_when_timer_close_to_expiring_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe 'As a user' do
+  describe 'with an dead man switch set' do
+    before :each do
+      yesterday = 1.days.ago
+      @user = create(:user)
+      @dms = DeadManSwitch.create(user_id: @user.id, interval_in_seconds: 86401,\
+        created_at: yesterday, updated_at: yesterday)
+      # require "pry"; binding.pry
+    end
+
+    it 'I receive a text message notification if it will expire within one day' do
+
+    end
+  end
+end
