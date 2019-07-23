ae99a5861f632340123815e9a6850c07cf59a190
Noah Flint <noah.flint24@gmail.com>
2019-07-23 14:46:45 -0600

    removed attachment spec

--- spec/features/users/upload_files_spec.rb
+++ spec/features/users/upload_files_spec.rb
@@ -19,20 +19,5 @@ RSpec.describe 'as a logged in user' do
         click_link 'Add A Will'
       end
     end
-
-    it 'can download file when attached' do
-      user = User.create(first_name: 'ricky', last_name: 'bobby', email: 'email@email.com', phone: '123')
-
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-
-      # this is tested in the user model
-      user.will.attach(io: File.open("./storage/1TestWill.pdf"), filename: "1TestWill.pdf", content_type: "pdf")
-
-      visit extra_info_path(user)
-
-      within '.will' do
-        expect(page).to have_link('Download Will Here')
-      end
-    end
   end
 end
