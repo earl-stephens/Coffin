914f89b6a15fdcfa601c5b8fabda4501924e57f6
earl-stephens <stephensearl@hotmail.com>
2019-07-22 21:43:44 -0600

    initial setup of connection from rails to sinatra
    Co-Authored-By: Ryan Miller <42855291+ryanmillergm@users.noreply.github.com>

--- app/controllers/dead_man_switch_controller.rb
+++ app/controllers/dead_man_switch_controller.rb
@@ -5,6 +5,13 @@ class DeadManSwitchController < ApplicationController
     @user = current_user
     @dead_man_switch = DeadManSwitch.find_or_create_by(user_id: @user.id)
     @dead_man_switch.update(interval_in_seconds: @length_of_time_for_switch)
+    data = {
+      "updated_at": "#{@updated_at}",
+      "interval": "#{@user.dead_man_switch.interval_in_seconds}",
+      "user_id": "#{@user.id}"
+    }
+    data = data.to_json
+    service(data)
     flash[:message] = "Your Dead Man Switch has been created and will expire on #{expiration_date}."
     redirect_to dashboard_path
   end
@@ -14,6 +21,14 @@ class DeadManSwitchController < ApplicationController
     @user.dead_man_switch.touch
     @user.dead_man_switch.one_day_message_sent = false
     @user.dead_man_switch.one_hour_message_sent = false
+    @updated_at = @user.dead_man_switch.updated_at.to_i
+    data = {
+      "updated_at": "#{@updated_at}",
+      "interval": "#{@user.dead_man_switch.interval_in_seconds}",
+      "user_id": "#{@user.id}"
+    }
+    data = data.to_json
+    service(data)
     flash[:message] = "Your timer has been reset and will expire on #{expiration_date}."
     redirect_to dashboard_path
   end
@@ -45,4 +60,16 @@ class DeadManSwitchController < ApplicationController
   def switch_params
     params.permit(:interval, :quantity)
   end
+
+def service(data)
+  response = Faraday.get('http://localhost:4567/timer2.json') do |f|
+    f.body = data
+    f.headers["Content-Type"] = "application/json",
+    f.headers["Content-Length"] = "16"
+  end
+  results = JSON.parse(response.body, symbolize_names: true)
+  # binding.pry
+
+end
+
 end
