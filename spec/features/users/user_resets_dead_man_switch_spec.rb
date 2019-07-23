29d265d7d25fb11a9c4f51a948b9fa5b8ed9157c
Brian Plantico <bplantico@gmail.com>
2019-07-23 13:05:48 -0600

    Refactors user feature specs for user setting and resetting dead man switches in profile and dashboard

--- spec/features/users/user_resets_dead_man_switch_spec.rb
+++ spec/features/users/user_resets_dead_man_switch_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe "As a logged in user" do
-  describe "and I visit the dashboard" do
+  describe "when I visit the dashboard" do
     describe "I see a button to activate my dead man switch" do
       before :each do
         @user_1 = User.create(first_name: "Bob", last_name: "Olsen", email: "bob@gmail.com")
@@ -9,7 +9,7 @@ RSpec.describe "As a logged in user" do
 
         allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user_1)
       end
-      it "I can activate my dead man switch" do
+      it "I can reset my dead man switch" do
         visit dashboard_path
 
         expect(page).to have_button("I'm Still Alive!")
