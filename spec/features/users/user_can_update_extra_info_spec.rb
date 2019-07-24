0340b7134aeebf32eb76d805b3169282348fdea7
Ryan Miller <ryanmillergm@gmail.com>
2019-07-24 11:24:31 -0600

    Fixed test for user can donate organs and user fills basic info

--- spec/features/users/user_can_update_extra_info_spec.rb
+++ spec/features/users/user_can_update_extra_info_spec.rb
@@ -4,16 +4,18 @@ RSpec.describe 'As a user' do
   describe 'I can update extra info in my profile' do
     before :each do
       @user1 = User.create(first_name: "Bob", last_name: "Olsen", email: "bob@gmail.com")
+
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user1)
     end
 
     it 'I can update organ donor to true' do
-      visit edit_info_path(@user1)
+      visit profile_path
 
       within('.organ_donor')
-      expect(page).to have_content('Organ Donor Status: false')
 
+      expect(page).to have_content('Organ Donor')
 
-      expect
+      choose('user[organ_donor]', option: "true")
     end
   end
 end
