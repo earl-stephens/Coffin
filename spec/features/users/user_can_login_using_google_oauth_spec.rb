0fecb3dab4b8567c6d88521555ea19c88afa7c08
Ryan Miller <ryanmillergm@gmail.com>
2019-07-24 15:06:43 -0600

    Adds user can login with google oauth

---
+++ spec/features/users/user_can_login_using_google_oauth_spec.rb
@@ -0,0 +1,37 @@
+require 'rails_helper'
+require 'vcr'
+
+RSpec.describe 'As a visitor' do
+  describe 'when I visit the welcome path' do
+    it 'I see a link to Login' do
+      visit '/'
+
+      expect(page).to have_link('Login using Google')
+    end
+
+    context 'and I click link to login using google and complete oauth process' do
+      it "I am taken to my dashboard" do
+
+        visit '/'
+
+        OmniAuth.config.test_mode = true
+
+        OmniAuth.config.mock_auth[:google_oauth2] = OmniAuth::AuthHash.new({
+          provider: 'google_oauth2',
+          credentials: { token: ENV['GOOGLE_TOKEN'] },
+          info: { first_name: "Ryan",
+                   last_name: "M",
+                   email: "ryanmillergm@gmail.com"}
+          })
+
+        click_link 'Register using Google'
+        click_link 'Logout'
+        click_link 'Login using Google'
+
+        user = User.last
+        save_and_open_page
+        expect(current_path).to eq(dashboard_path)
+      end
+    end
+  end
+end
