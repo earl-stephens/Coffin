8bf9a9a55b5f9174f568c0251d91c867351120a6
earl-stephens <stephensearl@hotmail.com>
2019-07-24 13:26:41 -0600

    wrote the start of the test
    Co-Authored-By: Brian Plantico <43261385+bplantico@users.noreply.github.com>

---
+++ spec/features/visitors/visitor_can_access_deceased_report_spec.rb
@@ -0,0 +1,46 @@
+require 'rails_helper'
+
+RSpec.describe 'As a primary contact for a user', type: :feature do
+  before :each do
+    @user = create(:user) # this user is alive at this point
+    first_name = 'Marsellus'
+    last_name = 'Wallace'
+    email = 'bplantico@gmail.com'
+    phone = '3038100629'
+    role = "Primary"
+
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+
+    visit new_profile_contact_path
+
+    fill_in 'contact[first_name]', with: first_name
+    fill_in 'contact[last_name]', with: last_name
+    fill_in 'contact[email]', with: email
+    fill_in 'contact[phone]', with: phone
+    fill_in 'contact[role]', with: role
+  end
+
+  it 'I receive an email to see the report for my user' do
+    
+    expect { click_on 'Add Contact' }.to change { ActionMailer::Base.deliveries.count }.by(1)
+
+    expect(current_path).to eq(dashboard_path)
+  end
+
+  it 'and when I click on the link in the email' do
+    click_on 'View Report'
+
+    contact = Contact.last
+
+    visit verified_email_path(verification_token: contact.verification_token)
+
+    expect(contact.verified). to be false
+
+    click_on 'Authenticated Login'
+    contact.reload
+
+    expect(current_path).to eq(report_path)
+    expect(contact.verified).to be true
+
+  end
+end
