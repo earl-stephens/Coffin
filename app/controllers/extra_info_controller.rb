d609cae2715051bf7bebf8fcdea9ad167a423dda
Noah Flint <noah.flint24@gmail.com>
2019-07-24 12:43:35 -0600

    added attacht params to update action for va bens and misc

--- app/controllers/extra_info_controller.rb
+++ app/controllers/extra_info_controller.rb
@@ -19,16 +19,21 @@ class ExtraInfoController < ApplicationController
     if params.has_key?('wills')
       @user.wills.attach(params['wills'])
       flash[:success] = "Your new will has been added!"
-    elsif params.has_key?('poa')
-      @user.poa.attach(params['poa'])
-      flash[:success] = "Your new POA has been added!"
-    elsif params.has_key?('birth_certificate')
-      @user.birth_certificate.attach(params['birth_certificate'])
+    elsif params.has_key?('powers_of_attorney')
+      @user.powers_of_attorney.attach(params['powers_of_attorney'])
+      flash[:success] = "Your new power of attorney has been added!"
+    elsif params.has_key?('birth_certificates')
+      @user.birth_certificates.attach(params['birth_certificates'])
       flash[:success] = "Your new birth certificate has been added!"
-    else params.has_key?('finance_records')
+    elsif params.has_key?('finance_records')
       @user.finance_records.attach(params['finance_records'])
       flash[:success] = "Your new financial record has been added!"
-
+    elsif params.has_key?('misc_files')
+      @user.misc_files.attach(params['misc_files'])
+      flash[:success] = "Your new file has been added!"
+    else params.has_key?('va_benefits')
+      @user.va_benefits.attach(params['va_benefits'])
+      flash[:success] = "Your VA file has been added!"
     end
     redirect_to extra_info_path(@user)
   end
