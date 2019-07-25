915f63bc6967f092ac91712b73a219ca1110ad92
Noah Flint <noah.flint24@gmail.com>
2019-07-24 21:21:21 -0600

    refactored def show action

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -1,12 +1,10 @@
 class ReportController < ApplicationController
 
   def show
-    # require 'pry'; binding.pry
     if current_user
       @user = current_user
     else
-    @user = User.find(5)
-    # @user = Contact.find(params[:id]).user
+    @user = Contact.find(params[:id]).user
     end
   end
 
