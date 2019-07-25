19f1e3a63e87876f64fa58ec34f9fc818ac8c3b3
earl-stephens <stephensearl@hotmail.com>
2019-07-24 22:51:24 -0600

    got twilio working with twilio gem and different environment variable names
    Co-Authored-By: Brian Plantico <43261385+bplantico@users.noreply.github.com>

--- app/controllers/notification_controller.rb
+++ app/controllers/notification_controller.rb
@@ -1,44 +1,51 @@
 class NotificationController < ApplicationController
-  # This controller queries the endpoint from the dead_man_switch miniservice
-  # and receive a time difference (as an integer) and a user id (as an integer).
-  # If the time difference is positive, then the switch has expired.
-  # If the time difference negative, then the switch has not expired
-  # 1 day = 24 hours = 1440 minutes = 86400 seconds
-  # one_day_plus_one_min = -86460 # i.e. this dms will expire in 1 day and 1 minutes
-  # one_day = -86400
-  # one_day_minues_one_min = -86340
 
   def message_sender
     @user = current_user
     @contact = current_user.find_primary_contact
     @user_email = @user.email
     @time_difference = session[:time_difference].to_i
-    @timer_one_day_message = "Coffin timer will expire in less than 24 hours."
-    @timer_one_hour_message = "Coffin timer will expire in less than 1 hour."
-    @timer_expired_message = "Coffin timer expired. Your info has been released."
-    # respond_to do |format|
-      # if @time_difference > 86400
-      #   @user.dead_man_switch.update(one_day_message_sent: false)
-      #   @user.dead_man_switch.update(one_hour_message_sent: false)
-      #   @user.dead_man_switch.update(expired_message_sent: false)
       if @time_difference <= 86415 && @user.dead_man_switch.one_day_message_sent == false
-        message = @timer_one_day_message
-        # TwilioTextMessenger.new(message).call # this is the syntax from Twilio to 'call' the message method
+        account_sid = ENV['EARLID']
+        auth_token = ENV['EARLTOKEN']
+
+        @client = Twilio::REST::Client.new(account_sid, auth_token)
+
+        message = @client.messages.create(
+                                     body: "Coffin timer will expire in less than 24 hours.",
+                                     from: '+13609791097',
+                                     to: '+13606203690'
+                                   )
+
         ContactNotifierMailer.day_warning(@user, @user_email).deliver_now
         @user.dead_man_switch.update(one_day_message_sent: true)
       elsif @time_difference <= 3600 && @user.dead_man_switch.one_hour_message_sent == false
-        message = @timer_one_hour_message
-        # TwilioTextMessenger.new(message).call
+        account_sid = ENV['EARLID']
+        auth_token = ENV['EARLTOKEN']
+        @client = Twilio::REST::Client.new(account_sid, auth_token)
+
+        message = @client.messages.create(
+                                     body: "Coffin timer will expire in less than 1 hour.",
+                                     from: '+13609791097',
+                                     to: '+13606203690'
+                                   )
+
         ContactNotifierMailer.hour_warning(@user, @user_email).deliver_now
         @user.dead_man_switch.update(one_hour_message_sent: true)
       elsif @time_difference <= 0 && @user.dead_man_switch.expired_message_sent == false
-        message = @timer_expired_message
-        # TwilioTextMessenger.new(message).call
+        account_sid = ENV['EARLID']
+        auth_token = ENV['EARLTOKEN']
+        @client = Twilio::REST::Client.new(account_sid, auth_token)
+
+        message = @client.messages.create(
+                                     body: "Coffin timer expired. Your info has been released.",
+                                     from: '+13609791097',
+                                     to: '+13606203690'
+                                   )
         ContactNotifierMailer.death_notice(@user, @contact).deliver_now
         @user.dead_man_switch.update(expired_message_sent: true)
         @user.update(deceased: true)
       end
       redirect_to dashboard_path
-    # end
   end
 end
