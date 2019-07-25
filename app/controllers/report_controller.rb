96e178e351321218978c1d16a705f043bbfbfa35
Noah Flint <noah.flint24@gmail.com>
2019-07-24 20:28:09 -0600

    added current_user to show action

--- app/controllers/report_controller.rb
+++ app/controllers/report_controller.rb
@@ -1,7 +1,13 @@
 class ReportController < ApplicationController
 
   def show
+    # require 'pry'; binding.pry
+    if current_user
+      @user = current_user
+    else
     @user = User.find(5)
+    # @user = Contact.find(params[:id]).user
+    end
   end
 
 end
